83e58ad25aa5fc7eb7afc7b6867e6c108a069a9b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2023-04-12 22:51:32 +0100

    Add 3 most recent ones on the main page

--- build/generate.py
+++ build/generate.py
@@ -7,23 +7,16 @@ SITEMAP_URLS = []
 ENV = get_jinja_env()
 CONTEXT = get_context()
 
-
-for page in ["index.html"]:
-    path = f"{BASE_FOLDER}/{page}"
-    print("Generating page:", path)
-    with open(path, "w") as f:
-        template = ENV.get_template(page)
-        f.write(template.render(**CONTEXT))
-
+# generate the daily emails
 source_files = sorted(glob.glob(f"./{CONTENT_FOLDER}/*.json"))
-dailies = []
+stories = []
 for i, daily_file in enumerate(source_files):
     with open(daily_file, "r") as f:
         parsed = json.loads(f.read())
     date = parsed.get("date").replace("/", "-")
-    permalink = f"https://unpack.tech/{date}"
+    parsed["permalink"] = f"https://unpack.tech/{date}"
     freshness = 0.25 + 0.75*i/len(source_files)
-    dailies.append(parsed)
+    stories.append(parsed)
     for destination, template, publish in [
         [f"{date}.html", "daily.html", True],
         [f"{date}-email.html", "email.html", False],
@@ -32,10 +25,19 @@ for i, daily_file in enumerate(source_files):
         print("Generating:", destination, "->", path, freshness)
         with open(path, "w") as f:
             template = ENV.get_template(template)
-            f.write(template.render(content=parsed, permalink=permalink, **CONTEXT))
+            f.write(template.render(content=parsed, **CONTEXT))
             if publish:
                 SITEMAP_URLS.append((destination, freshness))
-print("Parsed", len(dailies), "days")
+print("Parsed", len(stories), "days")
+
+# index
+for page in ["index.html"]:
+    path = f"{BASE_FOLDER}/{page}"
+    print("Generating page:", path)
+    recent = reversed(stories[-3:])
+    with open(path, "w") as f:
+        template = ENV.get_template(page)
+        f.write(template.render(stories=recent, **CONTEXT))
 
 # SITEMAP
 print("Generating sitemap.xml with %d items" % len(SITEMAP_URLS))
